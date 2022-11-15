323694251831921889e89b750bdfd6d3db55c938
MrLately <94589563+MrLately@users.noreply.github.com>
2022-11-15 13:44:38 -0800

    Create email.py

---
+++ email.py
@@ -0,0 +1,24 @@
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+email_sender = ''
+email_password = '' #if using gmail you must create and use an app password rather than your actual password.
+email_reciever = ''
+
+subject = ''
+body = """"""
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_reciever
+em['Subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_reciever, em.as_string())
+
+    print('Email Sent')
